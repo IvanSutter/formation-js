afd258255b0e9d6bef17d06dfc59ff79be4ce4bc
Ivan SUTTER <ivansutter@gmail.com>
2019-02-13 11:58:01 +0100

    ts

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const serveIndex = require('serve-index');
-const app = express();
-
-const www = 'www';
-
+var express = require('express');
+var serveIndex = require('serve-index');
+var app = express();
+var www = 'www';
 app.use(express.static(www));
 app.use(serveIndex(www, { view: "details", icons: true }));
-
 app.listen(3000, function () {
-  console.log('HO Example app listening on port 3000!')
+    console.log('HO Example app listening on port 3000!');
 });
