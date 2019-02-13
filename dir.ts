0304090e5eca45bf356384e08a8a1f67317d22da
Ivan SUTTER <ivansutter@gmail.com>
2019-02-13 17:17:38 +0100

    POST

--- dir.ts
+++ dir.ts
@@ -2,13 +2,16 @@ import * as express from 'express';
 
 const app = express.Router();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(`on est dans 'dir'`);
     next();
 });
 
 app.get('/date', (req, res, next) => {
-    res.json( {
+    res.json({
         date: (new Date()).toLocaleString('ja-JP')
     });
 });
@@ -19,6 +22,15 @@ app.get('/test', (req, res, next) => {
 
 app.get('/*', (req, res, next) => {
     console.log('oups');
+});
+
+app.post('/add', (req, res, next) => {
+    console.log('POST add');
+    const result = +req.body.a + +req.body.b;
+    console.log(result);
+    res.json({
+        result: result
+    });
 });
 
 export const dir = app;
