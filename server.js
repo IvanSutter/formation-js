00ef50a9e7e25adec36e3f1e8d2c7a92437afe16
Ivan SUTTER <ivansutter@gmail.com>
2019-02-13 15:18:35 +0100

    ch6

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var www = 'www';
 app.use(express.static(www));
 app.use(serveIndex(www, { view: "details", icons: true }));
 app.listen(3000, function () {
-    console.log('HO Example app listening on port 3000!');
+    console.log('HO js Example app listening on port 3000!');
 });
--- server.ts
+++ server.ts
@@ -1,7 +1,14 @@
 import * as express from 'express';
 import * as serveIndex from 'serve-index';
+import { timer, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const app = express();
+const o = timer(2000).pipe(
+  map(x => x)
+);
+
+o.subscribe(str => console.log('coucou', str));
 
 const www: string = 'www';
 
@@ -9,5 +16,5 @@ app.use(express.static(www));
 app.use(serveIndex(www, { view: "details", icons: true }));
 
 app.listen(3000, function () {
-  console.log('HOHO Example app listening on port 3000!')
+  console.log('HOHO TS Example app listening on port 3000!')
 });
